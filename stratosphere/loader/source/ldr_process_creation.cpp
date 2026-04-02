68e8d8b77a0c04486a1fdd4a62899d5edfd981bc
rashevskyv <rashevsky.v@gmail.com>
2026-04-02 09:42:27 +0000

    fixed oc

--- stratosphere/loader/source/ldr_process_creation.cpp
+++ stratosphere/loader/source/ldr_process_creation.cpp
@@ -373,6 +373,10 @@ namespace ams::ldr {
             R_UNLESS(meta->aci->program_id >= meta->acid->program_id_min, ldr::ResultInvalidProgramId());
             R_UNLESS(meta->aci->program_id <= meta->acid->program_id_max, ldr::ResultInvalidProgramId());
 
+            /* Check if nca is pcv or ptm */
+            g_is_pcv = meta->aci->program_id == ncm::SystemProgramId::Pcv;
+            g_is_ptm = meta->aci->program_id == ncm::SystemProgramId::Ptm;
+
             /* Validate the kernel capabilities. */
             R_TRY(TestCapability(static_cast<const util::BitPack32 *>(meta->acid_kac), meta->acid->kac_size / sizeof(util::BitPack32), static_cast<const util::BitPack32 *>(meta->aci_kac), meta->aci->kac_size / sizeof(util::BitPack32)));
 
